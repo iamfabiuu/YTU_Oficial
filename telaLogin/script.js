79ef73f61d4a607117a2f27bfb196d12789b65c8
FÁBIO ARAUJO SILVA <fabio.silva4645893@edu.pe.senac.br>
2026-09-03 20:00:07 -0300

    Voltamos

---
+++ telaLogin/script.js
@@ -0,0 +1,12 @@
+const splash = document.getElementById("splash");
+
+const login = document.getElementById("login");
+
+
+setTimeout(() => {
+
+    splash.style.display = "none";
+
+    login.style.display = "block";
+
+}, 5000);
